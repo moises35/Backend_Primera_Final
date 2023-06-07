36c040c7d5e7613d7be9229c131b5e761e58777e
Moisés Alvarenga <oscar-alvarenga35@fpuna.edu.py>
2023-06-07 15:07:57 -0400

    Add: Modelo de categoria

--- server/models/categoria.model.js
+++ server/models/categoria.model.js
@@ -1,17 +1,17 @@
-// Category.js
 const { DataTypes } = require('sequelize');
 const sequelize = require('./../configs/sequelize.config');
 
 const Category = sequelize.define('Category', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
-  },
-  nombre: {
-    type: DataTypes.STRING,
-    allowNull: false
-  }
+	id: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	nombre: {
+		type: DataTypes.STRING,
+		allowNull: false
+	}
 });
 
-module.exports = Category;
+module.exports = { Category };
